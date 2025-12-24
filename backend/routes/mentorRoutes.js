2a6f285935dd650344a6be24c7f71514eb7efd2e
SneaX-23 <sudeepmagdum321@gmail.com>
2025-12-24 19:27:02 +0530

    Add mentorship feature backend

---
+++ backend/routes/mentorRoutes.js
@@ -0,0 +1,35 @@
+import express from "express";
+import { 
+    createMentor, 
+    updateMentorProfile, 
+    browseMentorships, 
+    createMentee, 
+    handleApplication,
+    getMentorDashboard,
+    getStudentDashboard,
+    moderateMentorship
+} from "../controllers/mentorshipController.js";
+
+const router = express.Router();
+
+router.get("/browse", browseMentorships);
+
+// Apply for a mentorship (Mentee side)
+router.post("/apply/:mentorship_id", createMentee);
+
+// View- My Applications (Mentee side)
+router.get("/my-applications", getStudentDashboard);
+
+
+// --- Mentor Routes ---
+router.post("/profile", createMentor);
+router.patch("/profile", updateMentorProfile);
+router.get("/dashboard", getMentorDashboard);
+
+router.patch("/application/:enrollment_id", handleApplication);
+
+
+// --- Faculty/Admin Routes ---
+router.patch("/moderate/:mentorship_id", moderateMentorship);
+
+export default router;
